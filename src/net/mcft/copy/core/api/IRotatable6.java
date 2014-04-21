8888e12f43bf9f9491a607d2c0afc74ac2514746
copygirl <halfnerd.copygirl@gmail.com>
2014-04-21 13:23:11 +0200

    Clean up

--- src/net/mcft/copy/core/api/IRotatable6.java
+++ src/net/mcft/copy/core/api/IRotatable6.java
@@ -1,8 +1,5 @@
 package net.mcft.copy.core.api;
 
-import net.minecraft.world.IBlockAccess;
-import net.minecraftforge.common.util.ForgeDirection;
-
 /** When implemented in a Block, suggests it can be rotated in all 6 directions. */
 public interface IRotatable6 extends IRotatable4 {
 	
