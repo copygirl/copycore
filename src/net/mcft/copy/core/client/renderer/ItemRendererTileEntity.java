f070f0be225ef450ca0f7c6e12d12a33d7e641aa
copygirl <halfnerd.copygirl@gmail.com>
2014-05-28 01:35:24 +0200

    Update ItemRendererTileEntity
    More rotation options, improved rendering code

--- src/net/mcft/copy/core/client/renderer/ItemRendererTileEntity.java
+++ src/net/mcft/copy/core/client/renderer/ItemRendererTileEntity.java
@@ -9,6 +9,7 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraftforge.client.IItemRenderer;
 
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL12;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -21,7 +22,9 @@ public class ItemRendererTileEntity implements IItemRenderer {
 	
 	private float scale = 1;
 	private float offset = 0;
-	private float rotation = 0;
+	private float inventoryRotation = 0;
+	private float firstPersonRotation = 0;
+	private float thirdPersonRotation = 0;
 	
 	public ItemRendererTileEntity(Class<? extends TileEntity> tileEntityClass,
 	                              TileEntitySpecialRenderer renderer) {
@@ -38,8 +41,16 @@ public class ItemRendererTileEntity implements IItemRenderer {
 		this.offset = offset;
 		return this;
 	}
-	public ItemRendererTileEntity setRotation(float rotation) {
-		this.rotation = rotation;
+	public ItemRendererTileEntity setInventoryRotation(float rotation) {
+		this.inventoryRotation = rotation;
+		return this;
+	}
+	public ItemRendererTileEntity setFirstPersonRotation(float rotation) {
+		this.firstPersonRotation = rotation;
+		return this;
+	}
+	public ItemRendererTileEntity setThirdPersonRotation(float rotation) {
+		this.thirdPersonRotation = rotation;
 		return this;
 	}
 	
@@ -63,13 +74,20 @@ public class ItemRendererTileEntity implements IItemRenderer {
 		
 		GL11.glPushMatrix();
 		
-		if ((type != ItemRenderType.EQUIPPED) &&
-		    (type != ItemRenderType.EQUIPPED_FIRST_PERSON))
-			GL11.glTranslatef(0, -0.5F, 0);
+		boolean equippedFirstPerson = (type == ItemRenderType.EQUIPPED_FIRST_PERSON);
+		boolean equipped = ((type == ItemRenderType.EQUIPPED) || equippedFirstPerson);
+		boolean inventory = (type == ItemRenderType.INVENTORY);
 		
-		if (type == ItemRenderType.INVENTORY) {
-			GL11.glTranslatef(0, offset, 0);
+		if (equipped) {
+			GL11.glTranslatef(0.5F, 0, 0.5F);
+			float rotation = (equippedFirstPerson ? firstPersonRotation : (thirdPersonRotation - 90));
 			GL11.glRotatef(rotation, 0, -1, 0);
+		} else {
+			GL11.glTranslatef(0, -0.5F, 0);
+			if (inventory) {
+				GL11.glTranslatef(0, offset, 0);
+				GL11.glRotatef(inventoryRotation, 0, -1, 0);
+			}
 		}
 		
 		if (scale != 1.0F)
@@ -78,6 +96,7 @@ public class ItemRendererTileEntity implements IItemRenderer {
 		renderer.renderTileEntityAt(tileEntity, -0.5, 0, -0.5, 1);
 		
 		GL11.glPopMatrix();
+		GL11.glEnable(GL12.GL_RESCALE_NORMAL);
 		
 	}
 	
