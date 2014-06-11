abd30617b58ce24a0ff3d9bb564bceec2583f586
copygirl <halfnerd.copygirl@gmail.com>
2014-06-11 13:18:45 +0200

    Add getTimer and getPartialTicks to ClientUtils

--- src/net/mcft/copy/core/util/ClientUtils.java
+++ src/net/mcft/copy/core/util/ClientUtils.java
@@ -2,9 +2,15 @@ package net.mcft.copy.core.util;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.Timer;
 import net.minecraft.world.World;
+import cpw.mods.fml.relauncher.ReflectionHelper;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public final class ClientUtils {
+	
+	private static Timer mcTimer;
 	
 	private ClientUtils() {  }
 	
@@ -14,6 +20,17 @@ public final class ClientUtils {
 	
 	public static World getLocalWorld() {
 		return Minecraft.getMinecraft().theWorld;
+	}
+	
+	@SideOnly(Side.CLIENT)
+	public static Timer getTimer() {
+		return ((mcTimer == null) ? (mcTimer = ReflectionHelper.getPrivateValue(
+				Minecraft.class, Minecraft.getMinecraft(), "field_71428_T", "timer")) : mcTimer);
+	}
+	
+	@SideOnly(Side.CLIENT)
+	public static float getPartialTicks() {
+		return getTimer().renderPartialTicks;
 	}
 	
 }
