e01c9898210eec7f4befdcdc7fe6088faf7f33ed
copygirl <halfnerd.copygirl@gmail.com>
2015-04-15 20:31:03 +0200

    Add boolean support to EntityPropertyPrimitive

--- src/net/mcft/copy/core/entity/EntityPropertyPrimitive.java
+++ src/net/mcft/copy/core/entity/EntityPropertyPrimitive.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import net.mcft.copy.core.util.NbtUtils;
 import net.minecraft.nbt.NBTBase;
+import net.minecraft.nbt.NBTTagByte;
 import net.minecraft.network.PacketBuffer;
 
 public class EntityPropertyPrimitive<T> extends EntityProperty<T> {
@@ -14,12 +15,16 @@ public class EntityPropertyPrimitive<T> extends EntityProperty<T> {
 	
 	@Override
 	public NBTBase write() {
-		return NbtUtils.createTag(get());
+		T value = get();
+		if (value instanceof Boolean)
+			return new NBTTagByte((Boolean)value ? (byte)1 : (byte)0);
+		else return NbtUtils.createTag(get());
 	}
 	@Override
 	public void write(PacketBuffer buffer) {
 		T value = get();
 		if (value == null) throw new NullPointerException("value is null");
+		else if (value instanceof Boolean) buffer.writeBoolean((Boolean)value);
 		else if (value instanceof Byte) buffer.writeByte((Byte)value);
 		else if (value instanceof Short) buffer.writeShort((Short)value);
 		else if (value instanceof Integer) buffer.writeInt((Integer)value);
@@ -34,12 +39,16 @@ public class EntityPropertyPrimitive<T> extends EntityProperty<T> {
 	
 	@Override
 	public void read(NBTBase tag) {
-		set(NbtUtils.<T>getTagValue(tag));
+		T value = get();
+		if (value instanceof Boolean)
+			set((T)(Boolean)(NbtUtils.<Byte>getTagValue(tag) > 0));
+		else set(NbtUtils.<T>getTagValue(tag));
 	}
 	@Override
 	public void read(PacketBuffer buffer) {
 		T value = get();
 		if (value == null) throw new NullPointerException("value is null");
+		else if (value instanceof Boolean) set((T)(Object)buffer.readBoolean());
 		else if (value instanceof Byte) set((T)(Object)buffer.readByte());
 		else if (value instanceof Short) set((T)(Object)buffer.readShort());
 		else if (value instanceof Integer) set((T)(Object)buffer.readInt());
